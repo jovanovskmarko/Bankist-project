10396952bec1b22f25cb90005cc51ac576889ac1
markojovanovski1 <marko.jovanovski66@gmail.com>
2022-07-18 13:31:08 +0200

    Transfers and loands date added.

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,23 +29,21 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -78,7 +88,7 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 //CODING CHALLENGE
 
 //display functions
-
+let userLoggedIn;
 const displayMovements = function (userLoggedIn, sort = false) {
   const movements = userLoggedIn.movements;
 
@@ -86,8 +96,13 @@ const displayMovements = function (userLoggedIn, sort = false) {
     if (sort) return a - b;
     else return movements;
   });
-
+  containerMovements.textContent = '';
   movs.forEach(function (mov, i) {
+    const date = new Date(userLoggedIn.movementsDates[i]);
+    const year = `${date.getFullYear()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const day = date.getDate();
+    const displayDate = `${day}/${month}/${year}`;
     let type = '';
     if (mov > 0) {
       type = 'deposit';
@@ -99,7 +114,7 @@ const displayMovements = function (userLoggedIn, sort = false) {
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${
       i + 1
-    } ${type}</div>
+    } ${type}</div><div class="movements__date">${displayDate}</div>
       <div class="movements__value">${mov}€</div>
     </div>
   `;
@@ -125,7 +140,7 @@ const updateUI = function (user) {
 };
 
 //login event
-let userLoggedIn;
+
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
   const username = inputLoginUsername.value;
@@ -137,14 +152,11 @@ btnLogin.addEventListener('click', function (e) {
 
   if (userLoggedIn?.pin === Number(pin)) {
     //DISPLAY UI AND WELCOME MESSAGE
-
     labelWelcome.textContent = `Welcome ${userLoggedIn.owner.split(' ')[0]}`;
     containerApp.style.opacity = 100;
+    containerMovements.innerHTML = '';
+    updateUI(userLoggedIn);
   }
-
-  containerMovements.innerHTML = '';
-
-  updateUI(userLoggedIn);
 });
 
 //transfer event
@@ -164,6 +176,8 @@ btnTransfer.addEventListener('click', function (e) {
   ) {
     userLoggedIn.movements.push(0 - amount);
     receiverAcc.movements.push(amount);
+    receiverAcc.movementsDates.push(new Date().toISOString());
+    userLoggedIn.movementsDates.push(new Date().toISOString());
   }
 
   inputTransferAmount.value = inputTransferTo.value = '';
@@ -180,6 +194,7 @@ btnLoan.addEventListener('click', function (e) {
     })
   ) {
     userLoggedIn.movements.push(amount);
+    userLoggedIn.movementsDates.push(new Date().toISOString());
     updateUI(userLoggedIn);
     inputLoanAmount.value = '';
   }
@@ -209,3 +224,14 @@ btnSort.addEventListener('click', function (e) {
   displayMovements(userLoggedIn, !sorted);
   sorted = !sorted;
 });
+
+//FAKING LOG IN
+
+const now = new Date();
+const year = `${now.getFullYear()}`.padStart(2, 0);
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const day = now.getDate();
+const hour = now.getHours();
+const min = now.getMinutes();
+
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
